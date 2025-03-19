1eab49bdde4d7e11c07d1620dd27d3c168a42534
Krzysztof <patulekk.bsp@gmail.com>
2025-03-19 13:09:02 +0100

    Initial Commit

---
+++ climbcheck.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import argparse
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', required=True)
+parser.add_argument('--source', required=True)
+parser.add_argument('--thresh', default=0.7)
+args = parser.parse_args()
+
+model_path = args.model
+img_source = args.source
+min_thresh = float(args.thresh)
+
+if not os.path.exists(model_path):
+    print('ERROR: Model path is invalid.')
+    sys.exit(0)
+
+model = YOLO(model_path, task='detect')
+labels = model.names
+
+vid_ext_list = ['.avi', '.mov', '.mp4', '.mkv', '.wmv']
+
+if os.path.isfile(img_source):
+    _, ext = os.path.splitext(img_source)
+    if ext in vid_ext_list:
+        source_type = 'video'
+    else:
+        print(f'File extension {ext} is not supported.')
+        sys.exit(0)
+elif 'usb' in img_source:
+    source_type = 'usb'
+    usb_idx = int(img_source[3:])
+else:
+    print(f'Input {img_source} is invalid.')
+    sys.exit(0)
+
+if source_type == 'video':
+    cap = cv2.VideoCapture(img_source)
+elif source_type == 'usb':
+    cap = cv2.VideoCapture(usb_idx)
+
+bbox_colors = [(164, 120, 87), (68, 148, 228), (93, 97, 209), (178, 182, 133), (88, 159, 106)]
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, verbose=False)
+    detections = results[0].boxes
+
+    safe = True
+    for i in range(len(detections)):
+        xyxy = detections[i].xyxy.cpu().numpy().squeeze().astype(int)
+        xmin, ymin, xmax, ymax = xyxy
+        classidx = int(detections[i].cls.item())
+        classname = labels[classidx]
+        conf = detections[i].conf.item()
+
+        if conf > min_thresh:
+            color = bbox_colors[classidx % len(bbox_colors)]
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
+            label = f'{classname}: {int(conf * 100)}%'
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+            label_ymin = max(ymin, labelSize[1] + 10)
+            cv2.rectangle(frame, (xmin, label_ymin - labelSize[1] - 10), (xmin + labelSize[0], label_ymin + baseLine - 10), color, cv2.FILLED)
+            cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+            safe = True
+        else:
+            safe = False
+
+    cv2.imshow('YOLO detection results', frame)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
